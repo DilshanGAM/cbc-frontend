a788fe26e32f9807f4d0f9ace6659bc2b55e619a
Nipuna <IT21166556@my.sliit.lk>
2024-12-11 20:52:39 +0530

    implementing routes and useState

---
+++ src/components/productCard.jsx
@@ -0,0 +1,15 @@
+export default function ProductCard(props){
+
+  console.log(props)
+  
+  return (
+    <div>
+      <img src={props.src} />
+      <button>Add to cart</button>
+      <h1>{props.name}</h1>
+      <h2>Price: {props.price}</h2>      
+    </div>
+
+  )
+}
+
