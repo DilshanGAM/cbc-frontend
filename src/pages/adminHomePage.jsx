942e80d777355395248ca40995b10c6ad7cc6236
Nipuna <IT21166556@my.sliit.lk>
2024-12-25 21:17:27 +0530

    useEffect final part 👍

--- src/pages/adminHomePage.jsx
+++ src/pages/adminHomePage.jsx
@@ -1,6 +1,7 @@
 import { Link, Route, Routes } from "react-router-dom";
 import { BsGraphUp, BsBoxSeam, BsCart4, BsPeopleFill } from "react-icons/bs";
 import AdminProductsPage from "./admin/adminProductsPage";
+import AddProductForm from "./admin/addProductForm";
 
 export default function AdminHomePage() {
   return (
@@ -35,10 +36,11 @@ export default function AdminHomePage() {
         </Link>
       </div>
 
-      <div className="w-[80%] h-screen bg-red-600">
+      <div className="w-[80%] h-screen ">
         <Routes path="/*">
           <Route path="/dashboard" element={<h1>Dashboard</h1>} />
           <Route path="/products" element={<AdminProductsPage/>} />
+          <Route path="/products/addProduct" element={<AddProductForm/>} />
           <Route path="/orders" element={<h1>Orders</h1>} />
           <Route path="/customers" element={<h1>Customers</h1>} />
           <Route path="/*" element={<h1>404 not found the admin page</h1>}/>
